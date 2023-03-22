0d47e70075b816dbf4b086e241208eab5eb02020
Lauri Saarela <lauri.saarela@gofore.com>
2023-03-22 11:06:43 +0200

    mobile menu (dialog): useRef hack to not have menu items focused automatically, added back button for mobile users, minor layout changes

---
+++ components/layout/back-button.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+import { Button } from 'suomifi-ui-components';
+
+export default function BackButton() {
+  const router = useRouter();
+
+  if (['/', '/profile', '/company'].includes(router.route)) {
+    return null;
+  }
+
+  return (
+    <div className="block md:hidden px-4 -mb-2">
+      <Button
+        variant="secondaryNoBorder"
+        icon="arrowLeft"
+        className="!px-0"
+        onClick={() => router.back()}
+      >
+        BACK
+      </Button>
+    </div>
+  );
+}

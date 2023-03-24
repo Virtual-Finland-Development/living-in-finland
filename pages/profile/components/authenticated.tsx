b55398ac9d58c1aa48013f6ee64f03690c85bce8
Lauri Saarela <lauri.saarela@gofore.com>
2023-03-24 12:59:55 +0200

    ProfileAuthenticated layout

--- pages/profile/components/authenticated.tsx
+++ pages/profile/components/authenticated.tsx
@@ -7,36 +7,41 @@ import ProfileForm from './profile-form';
 
 export default function ProfileAuthenticated() {
   const { data: profile, isLoading, isFetching, error } = useProfile();
-
-  if (isLoading || isFetching) {
-    return <Loading />;
-  }
+  const loading = isLoading || isFetching;
 
   return (
     <>
-      <Page.Block className="bg-suomifi-blue-bg-light">
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-row items-center">
-            <StaticIcon icon="userProfile" className="h-16 w-16" />
-            <CustomHeading variant="h2" suomiFiBlue="dark">
-              Let’s create your profile
-            </CustomHeading>
-          </div>
-          <Text>
-            Please choose the best described options of yourself. This
-            information is under your control all the time and you will decide
-            to whom you want to share it.
-          </Text>
-        </div>
-      </Page.Block>
-      <Page.Block className="bg-white">
-        <div className="flex flex-col gap-6">
-          <CustomHeading variant="h2" suomiFiBlue="dark">
-            Personal information
-          </CustomHeading>
-          <ProfileForm profile={profile} />
-        </div>
-      </Page.Block>
+      {loading ? (
+        <Page.Block className="bg-white">
+          <Loading />
+        </Page.Block>
+      ) : (
+        <>
+          <Page.Block className="bg-suomifi-blue-bg-light">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-row items-center">
+                <StaticIcon icon="userProfile" className="h-16 w-16" />
+                <CustomHeading variant="h2" suomiFiBlue="dark">
+                  Let’s create your profile
+                </CustomHeading>
+              </div>
+              <Text>
+                Please choose the best described options of yourself. This
+                information is under your control all the time and you will
+                decide to whom you want to share it.
+              </Text>
+            </div>
+          </Page.Block>
+          <Page.Block className="bg-white">
+            <div className="flex flex-col gap-6">
+              <CustomHeading variant="h2" suomiFiBlue="dark">
+                Personal information
+              </CustomHeading>
+              <ProfileForm profile={profile} />
+            </div>
+          </Page.Block>
+        </>
+      )}
     </>
   );
 }
